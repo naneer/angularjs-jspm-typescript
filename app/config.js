6570444dbab60c043acdeeb6802eb024fdee469e
Reenan <reenan.arbitrario@gmail.com>
2015-03-26 23:32:36 +0000

    install angular-ui-router

--- app/config.js
+++ app/config.js
@@ -9,7 +9,11 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.3.15"
+    "angular": "github:angular/bower-angular@1.3.15",
+    "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
+    "github:angular-ui/ui-router@0.2.13": {
+      "angular": "github:angular/bower-angular@1.3.15"
+    }
   }
 });
 
