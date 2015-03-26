8595accf0e8df52566e998104da236745a641d3d
Reenan <reenan.arbitrario@gmail.com>
2015-03-26 19:41:26 +0000

    create index.html

--- app/config.js
+++ app/config.js
@@ -1,7 +1,14 @@
 System.config({
   "baseURL": "/",
   "paths": {
-    "*": "*.js"
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.3.15"
   }
 });
 
