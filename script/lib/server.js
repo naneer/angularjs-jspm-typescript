cd2fc5ce98f4a50bf811ffa1908e162b637c9b66
Reenan <reenan.arbitrario@gmail.com>
2015-03-26 20:25:16 +0000

    install serve-static in npm, create server start config file

---
+++ script/lib/server.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var http = require('http');
+var url = require('url');
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+
+var server = connect()
+    .use(serveStatic(options.root));
+
+
+http.createServer(server)
+    .listen(options.port)
+    .once('error', function(err){
+        if (err.code === 'EADDRINUSE') {
+            console.log('Port in use: %s', options.port);
+        } else {
+            console.error(err);
+        }
+    })
+    .on('listening', function(){
+        console.log('Started web server on http:/localhost:' + options.port);
+    })
+});
