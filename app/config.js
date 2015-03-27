203fd90f37cc12f601d4a9191c2ef40128bb184a
naneer <reenan.arbitrario@gmail.com>
2015-03-27 16:21:45 +0000

    install packages

--- app/config.js
+++ app/config.js
@@ -12,6 +12,8 @@ System.config({
     "angular": "github:angular/bower-angular@1.3.15",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
     "text": "github:systemjs/plugin-text@0.0.2",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "github:angular-ui/ui-router@0.2.13": {
       "angular": "github:angular/bower-angular@1.3.15"
     }
