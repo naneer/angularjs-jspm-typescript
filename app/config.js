d86801fdcd55f47d6e8de16f354d487c349360d2
naneer <reenan.arbitrario@gmail.com>
2015-04-01 23:34:39 +0000

    added angular-material

--- app/config.js
+++ app/config.js
@@ -10,6 +10,9 @@ System.config({
 System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.3.15",
+    "angular-animate": "npm:angular-animate@1.3.15",
+    "angular-aria": "npm:angular-aria@1.3.15",
+    "angular-material": "npm:angular-material@0.8.3",
     "angular-mocks": "github:angular/bower-angular-mocks@1.3.15",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
     "text": "github:systemjs/plugin-text@0.0.2",
@@ -20,6 +23,18 @@ System.config({
     },
     "github:angular/bower-angular-mocks@1.3.15": {
       "angular": "github:angular/bower-angular@1.3.15"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:angular-material@0.8.3": {
+      "angular": "npm:angular@1.3.15",
+      "angular-animate": "npm:angular-animate@1.3.15",
+      "angular-aria": "npm:angular-aria@1.3.15",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:angular@1.3.15": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
--- src/mainmodule.ts
+++ src/mainmodule.ts
@@ -1,12 +1,20 @@
 ///<reference path="../typings/tsd.d.ts" />
 ///<amd-dependency path="angular-ui-router" />
+///<amd-dependency path="angular-aria" />
+///<amd-dependency path="angular-animate" />
+///<amd-dependency path="angular-material" />
+
 import angular = require('angular');
 
 import testController = require('./testcontroller');
 
 
-var mainModule = angular.module('mainModule', ['ui.router']);
-  mainModule.controller(testController);
-
+var mainModule = angular.module('mainModule', [
+  'ui.router', 
+  'ngMaterial',  
+  'ngAnimate',
+  'ngAria']);
+ 
+mainModule.controller(testController);
 
 export = mainModule;
