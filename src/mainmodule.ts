c90fa2c2c3d4aac2ec20580a51ac1c09936faa4e
naneer <reenan.arbitrario@gmail.com>
2015-04-01 21:17:31 +0000

    amd-dependency

--- src/mainmodule.ts
+++ src/mainmodule.ts
@@ -1,7 +1,10 @@
 ///<reference path="../typings/tsd.d.ts" />
+///<amd-dependency path="angular-ui-router" />
 import angular = require('angular');
 
 import testController = require('./testcontroller');
-var mainModule = angular.module('mainModule', []);
-mainModule.controller(testController);
+var mainModule = angular.module('mainModule', ['ui.router']);
+  mainModule.controller(testController);
+
+
 export = mainModule;
